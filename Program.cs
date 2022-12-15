278f072129046c888c822dab02d515e5a76d0254
Theo <theodor.salla@gmail.com>
2022-12-15 21:38:56 +0100

    added duck class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DuckRace;
+
+Duck donald = new("Donald");
+System.Console.WriteLine(donald.ToString());
